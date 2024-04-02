681b6939c144618e5c4b132940f52d505e56f556
Wojciech Włodarczyk <wlodarczyk.woj@gmail.com>
2024-04-03 00:53:29 +0200

    Updated documentation

--- fajne_dane/urls.py
+++ fajne_dane/urls.py
@@ -2,26 +2,25 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
+from drf_yasg import openapi
 from drf_yasg.renderers import SwaggerUIRenderer
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from django.shortcuts import redirect
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
 import users.views
+
 
 schema_view = get_schema_view(
    openapi.Info(
       title="Fajne Dane API",
       default_version='v1.0.0',
-      description="Test description",
+      description="Dokumentacja REST API",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      license=openapi.License(name="<Nazwa licencji>"),
    ),
    public=False,
    permission_classes=(permissions.AllowAny,),
